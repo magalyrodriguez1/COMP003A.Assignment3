ddf5069b4e7b9cf04e6455070647705c1538e7f8
magalyrodriguez1 <142467680+magalyrodriguez1@users.noreply.github.com>
2024-02-04 16:25:01 -0800

    added switch statement

--- Program.cs
+++ Program.cs
@@ -12,12 +12,13 @@ namespace COMP003A.Assignment3
         {
             Console.WriteLine("Numeric to letter grade using an if-else statement");
             Console.WriteLine("Enter a numeric grade (0-100):");
+
             string gradeInput = Console.ReadLine();
-            double numericGrade = Convert.ToDouble(gradeInput);
+            double numericGrade = Convert.ToDouble(gradeInput); //converts string to double 
 
 
 
-            if (numericGrade >= 0 && numericGrade <= 100)
+            if (numericGrade >= 0 && numericGrade <= 100) //if condition for numbers 0-100; else will return invalid input
             {
 
                 if (numericGrade >= 90)
@@ -50,7 +51,27 @@ namespace COMP003A.Assignment3
             {
                 Console.WriteLine("Invalid input!");
             }
-            
+
+            Console.WriteLine("*************************************************");
+            Console.WriteLine("Integer to string day using a switch statement");
+            Console.WriteLine("Enter an integer day of the week (1-7): ");
+
+            string integerInput = Console.ReadLine();
+            int numericDay = Convert.ToInt32(integerInput); //converts string to int
+
+            switch(numericDay)
+            {
+                case 1:
+                    Console.WriteLine("String Day: Monday");
+                    break;
+
+                case 2: 
+            }
+
+
         }
+             
+
+
     }
 }
